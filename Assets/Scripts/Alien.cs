1aa88b321feaa8822550e40d95ae504150989a9c
WemkoDijkhuis <wemko.dijkhuis@trivento.nl>
2020-11-15 15:51:59 +0100

    moving enemies

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    public float moveDelay = 1.0f;
+    public float moveSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(MoveCoroutine());
+    }
+
+    private void Move()
+    {
+        Debug.Log("Moving");
+        transform.Translate(new Vector2(moveSpeed, 0));
+    }
+
+    private IEnumerator MoveCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(moveDelay);
+            Move();
+        }
+    }
+}
