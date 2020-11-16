6c4f58924225246131d7b03b7f9d2dc094b374be
WemkoDijkhuis <wemko.dijkhuis@trivento.nl>
2020-11-16 15:27:47 +0100

    Step 2

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 8.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(0, speed * Time.deltaTime));
+
+        if (transform.position.y < -100)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,18 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float speed = 8.0f;
+    [SerializeField] private GameObject bulletPrefab;
 
     // Update is called once per frame
     void Update()
     {
         float axisX = Input.GetAxis("Horizontal");
         transform.Translate(new Vector2(axisX * speed * Time.deltaTime, 0));
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject instantiated = Instantiate(bulletPrefab);
+            instantiated.transform.position = transform.position;
+        }
     }
 }
