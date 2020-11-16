d4a342eb2a89429dd757e885ad68198d0882132f
WemkoDijkhuis <wemko.dijkhuis@trivento.nl>
2020-11-16 15:18:42 +0100

    Step 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 8.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axisX = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(axisX * speed * Time.deltaTime, 0));
+    }
+}
